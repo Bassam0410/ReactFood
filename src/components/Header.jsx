9f3b78240741a6d62f1fec7269a93f82ebe847de
Bassam0410 <alshujaabassam0410@gmail.com>
2026-01-04 19:00:11 +0000

    update the logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Logo from "../assets/logo.jpg"
 import List from './shopList/List'
 import Form from './Checkout/Form'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -6,7 +7,7 @@ function Header(props) {
     return (
         <div id='main-header' >
             <div id="title">
-                <img src="public/logo.jpg" alt="image" />
+                <img src= {Logo} alt="image" />
                 <h2>ReactFood</h2>
             </div>
             <h1 className='text-button' onClick={() => {
